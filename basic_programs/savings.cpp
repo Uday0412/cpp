ca4e77542223ed44ee379660398b8ab95aaddc93
Uday0412 <kushwaha549786@gmail.com>
2024-04-05 22:54:09 +0530

    find savings

---
+++ basic_programs/savings.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int saving;
+    cout<<"enetr the savings=";
+    cin>>saving;
+
+    if(saving>5000)
+    {
+        cout<<"neha";
+    }
+    else if(saving>3000)
+    {
+        cout<<"rashmi";
+    }
+    else
+    {
+        cout<<"friends;"
+    }
+    return 0;
+}
