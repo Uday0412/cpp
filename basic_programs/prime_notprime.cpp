f4c43eba4694ca3155f2b826204e13e4c43d015a
Uday0412 <kushwaha549786@gmail.com>
2024-04-05 22:50:08 +0530

    prime OR prime

---
+++ basic_programs/prime_notprime.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int i;
+    for(i=2;i<n;i++)
+    {
+        if(n%i==0)
+        {
+            cout<<"not prime";
+            break;
+        }
+    }
+    if(i==n)
+    {
+        cout<<"prime";
+    }
+    return 0;
+}
