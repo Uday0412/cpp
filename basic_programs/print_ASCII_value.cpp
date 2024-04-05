c4af6931e0ca3d62662e983ee05460e143941964
Uday0412 <kushwaha549786@gmail.com>
2024-04-05 22:51:27 +0530

    print ASCII value

---
+++ basic_programs/print_ASCII_value.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+    char a,z;                                                                                                                          
+    for(i=a;i<=z;i++)
+    {
+        cout<<(char)i<<"\n";
+    }
+    return 0;
+}
