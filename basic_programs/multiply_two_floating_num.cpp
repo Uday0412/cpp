abd7d343f9bc1f5e7c3e41764e2cd7b69da622df
Uday0412 <kushwaha549786@gmail.com>
2024-04-05 22:48:54 +0530

    multiply two floting numbers

---
+++ basic_programs/multiply_two_floating_num.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    float a;
+    cout<<"enter the number a";
+    cin>>a;
+
+    float b;
+    cout<<"enetr the number b";
+    cin>>b;
+
+    cout<<"multiply ="<<a*b;
+    return 0;
+}
