cb5b69413bc47dce3184492ae5dffdba0ff4d3bf
Uday0412 <kushwaha549786@gmail.com>
2024-04-07 19:02:10 +0530

    sum of numbers

---
+++ basic_programs/sum.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"enetr the number A: ";
+    cin>>a;
+    int b;
+    cout<<"eneter the number B: ";
+    cin>>b;
+
+    int sum=a+b;
+    cout<<"sum = "<<sum;
+    return 0;
+}
