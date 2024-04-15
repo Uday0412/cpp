5bf3e4ca13604068339cee415ca82dbed35d165b
Uday0412 <kushwaha549786@gmail.com>
2024-04-15 14:23:38 +0530

    180 rotation using cpp

---
+++ patteran_printing/floyd_triangle.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+
+    int cout=1;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            cout<<count<<" ";
+            cout++;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
